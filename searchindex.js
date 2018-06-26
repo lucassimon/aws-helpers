41d24e038eadd69e09e5ae00f80d741caa8f55bd
g.punter <g.punter@bboxx.co.uk>
2018-06-26 14:32:22 +0100

    First commit of autogen docs

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["README","index"],envversion:53,filenames:["README.md","index.rst"],objects:{},objnames:{},objtypes:{},terms:{aws:[0,1],boto3:0,helper:[0,1],index:1,modul:1,page:1,project:1,search:1,some:0,work:0},titles:["aws_helpers","python-docs"],titleterms:{aws_help:0,content:1,doc:1,indic:1,python:1,tabl:1}})
