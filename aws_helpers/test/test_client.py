93076ddb87b34d441e4a1ebf8000be2755be0ae9
g.punter <g.punter@bboxx.co.uk>
2018-06-27 12:09:26 +0100

    fixing tests

--- aws_helpers/test/test_client.py
+++ aws_helpers/test/test_client.py
@@ -7,7 +7,7 @@ from aws_helpers.test import REGION
 
 class AccessKeysTest(unittest.TestCase):
     def test_load_access_keys(self):
-        access_key, secret_key = load_access_keys(access_key_path='.testkey.json')
+        access_key, secret_key = load_access_keys(access_key_path='testfiles/testkey.json')
         self.assertEqual('ABC',access_key)
         self.assertEqual('DEF',secret_key)
 
@@ -19,19 +19,16 @@ class ClientTest(unittest.TestCase):
         # Invalid region
         bad_region = Client('EU (Ireland)')
         # Invalid keys
-        bad_keys = Client(REGION, lambda: load_access_keys('.testkey.json'))
+        bad_keys = Client(REGION, lambda: load_access_keys('testfiles/testkey.json'))
 
     def test_get_session(self):
         client = Client(REGION)
         sess = client.get_session()
-        # check that sess is correct
-        print(dict(sess), type(sess))
 
     def test_get_resource(self):
         client = Client(REGION)
         # Check that resource you get has correct boto3 type
         s3 = client.get_resource('s3')
-        print(dict(s3), type(s3))
         
 
 
